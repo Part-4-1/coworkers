f35f36bac6bdd637a93aeeb0ffaf5468ad784d2a
Imhwitae <ghkdgnlxo2@gmail.com>
2025-10-30 00:00:57 +0900

    setting: 프로젝트 기본 세팅

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="flex">
+        <p className="ext-3xl font-bold underline">tailwind</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/index.ts
@@ -0,0 +1,2 @@
+// barrel 패턴을 위한 파일
+// ex) export { default as Button } from "./button"
