00e660c79fcacdc304f46372b15fbbafe5b3a091
Imhwitae <ghkdgnlxo2@gmail.com>
2025-10-30 16:31:45 +0900

    setting: clsx 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <div className="flex">
-        <p className="ext-3xl font-bold underline">tailwind</p>
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 }
