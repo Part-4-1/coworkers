05ac847720dc10b78fb1ed118e026cbfd7edd35c
apple <aa@aui-MacBookPro.local>
2025-11-03 19:26:46 +0900

    chore: 충돌수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 // barrel 패턴을 위한 파일
 // ex) export { default as Button } from "./button"
 export { default as TextInput } from "./input-components/text-input";
+export { default as SidebarDropdown } from "./sidebar-components/sidebar-dropdown";
