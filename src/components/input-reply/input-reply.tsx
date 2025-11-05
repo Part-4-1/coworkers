6551f66a8f6744a5b63b424c60298d44ee9ccf25
junye0l <junyeolkim00@gmail.com>
2025-11-06 01:08:24 +0900

    feat : 인풋안에 값이 있을때 버튼 배경색 변경

--- src/components/input-reply/input-reply.tsx
+++ src/components/input-reply/input-reply.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+import cn from "@/utils/clsx";
 import { Button, Icon } from "@/components/index";
 import TextareaAutosize from "react-textarea-autosize";
 
@@ -7,17 +11,28 @@ import TextareaAutosize from "react-textarea-autosize";
  */
 
 const InputReply = () => {
+  const [value, setValue] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className="flex w-full max-w-[732px] flex-col border-y-2 border-gray-300 py-3">
       <div className="gap-6 pl-3 pr-3 flex-center">
         <TextareaAutosize
           placeholder="댓글을 달아주세요"
+          value={value}
+          onChange={handleChange}
           className="w-full max-w-[708px] resize-none text-xs text-blue-700 placeholder:text-gray-800 focus:outline-none tablet:text-md"
           minRows={1}
         />
         <Button
           variant="none"
-          className="h-[24px] w-[24px] flex-shrink-0 rounded-full bg-gray-800 hover:bg-blue-100"
+          disabled={!value}
+          className={cn(
+            "h-[24px] w-[24px] flex-shrink-0 rounded-full",
+            value ? "bg-blue-100" : "bg-gray-800"
+          )}
         >
           <Icon icon="upArrow" className="h-4 w-4" />
         </Button>
