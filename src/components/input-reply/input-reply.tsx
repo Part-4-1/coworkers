4f7fcb0cc38cbcf67ebad9abee72dbac9a9f3df2
junye0l <junyeolkim00@gmail.com>
2025-11-05 23:10:55 +0900

    refactor : 코드 리뷰 반영

--- src/components/input-reply/input-reply.tsx
+++ src/components/input-reply/input-reply.tsx
@@ -13,14 +13,14 @@ const InputReply = () => {
       <div className="gap-6 pl-3 pr-3 flex-center">
         <TextareaAutosize
           placeholder="댓글을 달아주세요"
-          className="w-full max-w-[708px] resize-none text-xs text-blue-700 placeholder:text-gray-800 tablet:text-md pc:text-md"
+          className="w-full max-w-[708px] resize-none text-xs text-blue-700 placeholder:text-gray-800 tablet:text-md"
           minRows={1}
         />
         <Button
           variant="none"
           className="h-[24px] w-[24px] flex-shrink-0 rounded-full bg-gray-800 hover:bg-blue-100"
         >
-          <Icon icon="upArrow" width={16} height={16} />
+          <Icon icon="upArrow" className="h-4 w-4" />
         </Button>
       </div>
       <hr className="mt-3 bg-gray-300" />
