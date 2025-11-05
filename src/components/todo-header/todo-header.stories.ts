1df3d1ba9b659a211f723668646355780844808c
Imhwitae <ghkdgnlxo2@gmail.com>
2025-11-05 13:34:06 +0900

    chore: 스토리북 수정

--- src/components/todo-header/todo-header.stories.ts
+++ src/components/todo-header/todo-header.stories.ts
@@ -19,6 +19,9 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     todoName: "할 일",
+    btnClick: () => {
+      alert("할 일 추가 버튼 클릭됨");
+    },
   },
 };
 
@@ -26,5 +29,8 @@ export const WithLongText: Story = {
   args: {
     todoName:
       "오늘 할 일 목록 - 매우 긴 텍스트 예시로서, 이 텍스트가 어떻게 잘리는지 확인하기 위한 용도입니다. 스크린 너비를 초과할 경우 어떻게 표시될까요? 이 텍스트는 의도적으로 길게 작성되었습니다.",
+    btnClick: () => {
+      alert("할 일 추가 버튼 클릭됨");
+    },
   },
 };
