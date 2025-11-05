9d5393572e933c4cfc95c649f0138ccac4ee3ed9
JinHyuk Kim <dog33027@gmail.com>
2025-11-05 15:32:19 +0900

    feat: isCircle prop추가, 이미지 next/image 로 변경

--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,37 +1,46 @@
-import Icon from "@/components/icon/Icon";
+import { Icon } from "@/components/index";
 import cn from "@/utils/clsx";
+import Image from "next/image";
 
 /**
  * @author jinhyuk
  * @description 프로필 이미지를 보여주는 컴포넌트입니다.
  * @param image - 이미지 URL, 없을시 기본 이미지
  * @param size - "lg", "md", "sm" 프로필 크기 옵션
-
+ * @param isCircle = true 일떈 원형, false일시 사이즈별 radius 적용
  */
 
 interface ProfileProps {
   image?: string;
   size?: "lg" | "md" | "sm";
   className?: string;
+  isCircle?: boolean;
 }
 
-const Profile = ({ image, size = "lg", className }: ProfileProps) => {
+const Profile = ({
+  image,
+  size = "lg",
+  className,
+  isCircle = false,
+}: ProfileProps) => {
   const profileSize = {
     lg: 40,
     md: 32,
     sm: 24,
   }[size];
 
-  const profileRadius = {
-    lg: "rounded-[12px]",
-    md: "rounded-[8px]",
-    sm: "rounded-[6px]",
-  }[size];
+  const profileRadius = isCircle
+    ? "rounded-full"
+    : {
+        lg: "rounded-[12px]",
+        md: "rounded-[8px]",
+        sm: "rounded-[6px]",
+      }[size];
 
   return (
     <div className={cn(className)}>
       {image ? (
-        <img
+        <Image
           src={image}
           alt="프로필"
           width={profileSize}
