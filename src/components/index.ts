41796760c7dbfaf24c07ae0d9c667703cd4fe501
apple <aa@aui-MacBookPro.local>
2025-11-08 11:39:03 +0900

    chore:배럴패턴 위치 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -11,6 +11,6 @@ export { default as Badge } from "./badge/badge";
 export { default as InputReply } from "./input-reply/input-reply";
 export { default as InputBox } from "./input-box/input-box";
 export { default as GnbWrapper } from "./gnb/gnb-wrapper";
-export { default as TaskChip } from "./task-chip/task-chip";
 export { default as TaskHeader } from "./task-header/task-header";
+export { default as TaskChip } from "./task-chip/task-chip";
 export { default as ImageUpload } from "./image-upload/image-upload";
