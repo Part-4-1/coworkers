b4fa5e648e16ff1b4a7a3b32b8cda18948793d0d
Imhwitae <ghkdgnlxo2@gmail.com>
2025-11-09 15:22:23 +0900

    feat: date-util 함수 추가

--- src/utils/date-util.ts
+++ src/utils/date-util.ts
@@ -12,3 +12,26 @@ export const toDotDateString = (timestamp: string) => {
 
   return `${year}. ${month}. ${day}`;
 };
+
+export const toKoreanDateString = (timestamp: string) => {
+  const date = new Date(timestamp);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1);
+  const day = String(date.getDate());
+
+  return `${year}년 ${month}월 ${day}일`;
+};
+
+/**
+ * @author hwitae
+ * @param timestamp 타임스탬프
+ * @description 타임스탬프 날짜 형식을 0000년 00월 00일 오전/오후 0:00 형태로 변경합니다.
+ * @returns string
+ */
+export const toKoreanDateWithTimeString = (timestamp: string) => {
+  const date = new Date(timestamp);
+  const hour = date.getHours();
+  const min = date.getMinutes();
+
+  return `${toKoreanDateString(timestamp)} ${hour >= 12 ? `오후 ${hour - 12}` : `오전 ${hour}`}:${min}`;
+};
