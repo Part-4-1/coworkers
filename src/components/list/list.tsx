34fa3b46c5b79cafbbe207109814ac95d71ccb25
apple <aa@aui-MacBookPro.local>
2025-11-09 22:00:01 +0900

    chore:pr리뷰 반영

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Button, Checkbox, Dropdown, Icon } from "@/components/index";
-import { mockListData } from "@/mocks/list-data";
 import { Frequency_Map, type Task } from "@/types/task";
 import cn from "@/utils/clsx";
 import { toKoreanDateString } from "@/utils/date-util";
@@ -23,19 +22,23 @@ const List = ({
   return (
     <div
       className={cn(
-        "flex w-full max-w-[343px] flex-col gap-[10px] rounded-lg border border-gray-300 bg-white px-[14px] py-3 hover:bg-gray-100 tablet:max-w-[733px] pc:max-w-[1200px]",
+        "mx-auto flex w-full max-w-[343px] flex-col gap-[10px] rounded-lg border border-gray-300 bg-white px-[14px] py-3 hover:bg-gray-100 tablet:max-w-[733px] pc:max-w-[1200px]",
         className
       )}
     >
       <div className="flex justify-between">
-        <div className="flex gap-3 flex-center">
+        <div className="gap-3 flex-center">
           <Checkbox id={id} isDone={doneAt} taskName={name} />
-          <div className="flex items-center gap-[2px]">
-            <Button variant="none" onClick={() => {}}>
-              <Icon icon="comment" className="h-4 w-4 text-gray-800" />
+          {!!commentCount && (
+            <Button
+              variant="none"
+              className="flex items-center gap-[2px]"
+              onClick={() => {}}
+            >
+              <Icon icon="comment" className="h-4 w-4 text-gray-800" />{" "}
+              <span className="text-xs text-gray-800">{commentCount}</span>
             </Button>
-            <span className="text-xs text-gray-800">{commentCount}</span>
-          </div>
+          )}
         </div>
         <Dropdown
           trigger={
@@ -59,15 +62,13 @@ const List = ({
             {toKoreanDateString(date)}
           </span>
         </div>
-        {repeatPeriod !== null && (
-          <>
-            <hr className="h-3 w-[1px] bg-blue-600" />
-            <div className="flex items-center gap-[6px]">
-              <Icon icon="repeat" className="h-3 w-3 tablet:h-4 tablet:w-4" />
-              <span className="text-xs text-gray-800">{repeatPeriod}</span>
-            </div>
-          </>
-        )}
+        <>
+          <span className="h-3 w-[1px] bg-blue-600" />
+          <div className="flex items-center gap-[6px]">
+            <Icon icon="repeat" className="h-3 w-3 tablet:h-4 tablet:w-4" />
+            <span className="text-xs text-gray-800">{repeatPeriod}</span>
+          </div>
+        </>
       </div>
     </div>
   );
