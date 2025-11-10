9ed63c737fb4903ca06211d176a18407508508d8
junye0l <junyeolkim00@gmail.com>
2025-11-10 22:15:59 +0900

    refactor : 불필요한 로그출력 제거

--- src/hooks/api/task/use-create-task.ts
+++ src/hooks/api/task/use-create-task.ts
@@ -20,7 +20,6 @@ export const useCreateTask = (params: UseCreateTaskParams) => {
       toast.success("할 일 생성에 성공했습니다.");
     },
     onError: (error) => {
-      console.error("Task creation failed:", error);
       toast.error("할 일 생성에 실패했습니다.");
     },
   });
