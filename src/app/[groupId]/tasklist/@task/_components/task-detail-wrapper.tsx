3a3c9ebdd7998d0b6ae233e3bb37e63625c9108b
Imhwitae <ghkdgnlxo2@gmail.com>
2025-11-11 16:46:50 +0900

    design: 페이지 애니메이션 적용

--- src/app/[groupId]/tasklist/@task/_components/task-detail-wrapper.tsx
+++ src/app/[groupId]/tasklist/@task/_components/task-detail-wrapper.tsx
@@ -1,23 +1,58 @@
 import { Button, Icon } from "@/components";
 import cn from "@/utils/clsx";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
 
+const pageVariants = {
+  initial: {
+    x: "100%",
+    opacity: 0,
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: "tween",
+      ease: "easeOut",
+      duration: 0.5,
+    } as const,
+  },
+  exit: {
+    x: "100%",
+    opacity: 0,
+    transition: {
+      type: "tween",
+      ease: "easeIn",
+      duration: 0.5,
+    } as const,
+  },
+};
+
 const TaskDetailWrapper = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+
   return (
-    <div
-      className={cn(
-        "flex w-full min-w-[375px] flex-col px-7 py-3",
-        "tablet:max-w-[520px] tablet:gap-4 tablet:pt-10",
-        "pc:max-w-[780px] pc:gap-5"
-      )}
-    >
-      <div className="flex flex-col gap-5 tablet:gap-[15px] pc:gap-[74px]">
-        <Button variant="none">
-          <Icon icon="x" className="h-6 w-6" />
-        </Button>
-        {children}
-      </div>
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        variants={pageVariants}
+        initial="initial"
+        animate="visible"
+        exit="exit"
+        className={cn(
+          "flex w-full min-w-[375px] flex-col px-7 py-3",
+          "tablet:max-w-[520px] tablet:gap-4 tablet:pt-10",
+          "pc:max-w-[780px] pc:gap-5"
+        )}
+      >
+        <div className="flex flex-col gap-5 tablet:gap-[15px] pc:gap-[74px]">
+          <Button variant="none" onClick={() => router.back()}>
+            <Icon icon="x" className="h-6 w-6" />
+          </Button>
+          {children}
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
