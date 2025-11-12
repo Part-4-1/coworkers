498e257a42f68ed0f796465706c1868a5472056e
Imhwitae <ghkdgnlxo2@gmail.com>
2025-11-12 17:28:21 +0900

    chore: 코드, 파일 정리

--- src/app/[groupId]/tasklist/@task/page.tsx
+++ src/app/[groupId]/tasklist/@task/page.tsx
@@ -2,16 +2,12 @@
 
 import TaskDetailComment from "./_components/task-detail-comment";
 import TaskDetailContents from "./_components/task-detail-contents";
-import data from "@/mocks/task-detail-data.json";
-import { mockComments } from "@/mocks/comment-data";
 import { InputReply } from "@/components";
 import TaskDetailWrapper from "./_components/task-detail-wrapper";
-import TaskDetailToggleBtn from "./_components/task-detail-complete-btn";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import useGetTaskDetail from "@/hooks/api/task/use-get-task-detail";
 import { AnimatePresence } from "framer-motion";
-import useGetComments from "@/hooks/api/comments/use-get-comments";
 import { useCreateComment } from "@/hooks/api/comments/use-create-comment";
 
 const Page = () => {
@@ -29,16 +25,13 @@ const Page = () => {
   };
 
   const { data: taskDetailData, isPending } = useGetTaskDetail(
+    // TODO: groupId, taskListId 동적으로 받아올 수 있도록 수정
     3290,
     4711,
     taskId
   );
 
   const { mutate: postTaskDetailComment } = useCreateComment(taskId);
-
-  useEffect(() => {
-    // console.log(comments);
-  });
 
   return (
     <AnimatePresence mode="wait" onExitComplete={handleClose}>
