9f12d42e6440dd52f2f5dce68594765a2fe2f7ba
apple <aa@aui-MacBookPro.local>
2025-11-12 22:34:05 +0900

    fix: useGetUserInfoQuery 무한 호출 버그 수정 (axios 인터셉터와  충돌)

--- src/hooks/api/user/use-get-user-info-query.ts
+++ src/hooks/api/user/use-get-user-info-query.ts
@@ -1,14 +1,23 @@
 import { getUserInfo } from "@/api/user/get-user-info";
+import { getCookie } from "@/utils/getCookie";
 import { useQuery } from "@tanstack/react-query";
-
+import { AxiosError } from "axios";
 /**
  * author leohan
  * 현재 로그인된 유저의 정보를 가져오는 훅
  */
 
 export const useGetUserInfoQuery = () => {
+  const accessToken = getCookie("accessToken");
   return useQuery({
     queryKey: ["userInfo"],
     queryFn: getUserInfo,
+    enabled: !!accessToken,
+    retry: (failCount, error) => {
+      if ((error as AxiosError)?.response?.status === 401) {
+        return false;
+      }
+      return failCount < 2;
+    },
   });
 };
