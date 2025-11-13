9f67047cc5bbfa953567217fdb50de6558518961
Imhwitae <ghkdgnlxo2@gmail.com>
2025-11-13 11:55:08 +0900

    chore: 코드 정리

--- src/app/[groupId]/tasklist/page.tsx
+++ src/app/[groupId]/tasklist/page.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import { DatePicker } from "@/components";
-
 const Page = () => {
-  return (
-    <div className="flex-col-center">
-      리스트 페이지
-      <DatePicker day="목" date={13} />
-      <DatePicker day="금" date={14} />
-    </div>
-  );
+  return <div className="flex-col-center">리스트 페이지</div>;
 };
 
 export default Page;
