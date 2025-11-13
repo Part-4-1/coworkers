60020ed81ab8f77e1e83b824746545e41bcec88d
junye0l <junyeolkim00@gmail.com>
2025-11-13 14:40:03 +0900

    design : 각 헤더 글씨체 굵기 수정

--- src/app/boards/_components/boards-best/boards-best-header/boards-best-header.tsx
+++ src/app/boards/_components/boards-best/boards-best-header/boards-best-header.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@/components/index";
 const BoardsBestHeader = () => {
   return (
     <div className="flex items-center justify-between">
-      <h2 className="m-0 text-xl font-bold">베스트 게시글</h2>
+      <h2 className="m-0 text-xl font-semibold">베스트 게시글</h2>
       <div className="gap-[2px] flex-center">
         <span className="text-sm text-gray-700">더보기</span>
         <Icon className="h-4 w-4" icon="rightArrow" />
--- src/app/boards/_components/boards-header/boards-header.tsx
+++ src/app/boards/_components/boards-header/boards-header.tsx
@@ -3,7 +3,7 @@ import { TextInput, Button, Icon } from "@/components/index";
 const BoardsHeader = () => {
   return (
     <div className="mx-auto mb-[30px] mt-[87px] flex w-full items-center justify-between tablet:max-w-[620px] pc:max-w-[1120px]">
-      <h1 className="text-2xl font-bold">자유게시판</h1>
+      <h1 className="text-2xl font-semibold">자유게시판</h1>
       <TextInput
         className="rounded-full border-2"
         placeholder="검색어를 입력해주세요"
