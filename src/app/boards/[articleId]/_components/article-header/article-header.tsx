da5993d067cfac2e0fbdc3ffc148a0549ce95500
junye0l <junyeolkim00@gmail.com>
2025-11-16 00:49:37 +0900

    feat : 게시글 드롭다운 연결

--- src/app/boards/[articleId]/_components/article-header/article-header.tsx
+++ src/app/boards/[articleId]/_components/article-header/article-header.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import Image from "next/image";
-import { Button, Icon } from "@/components/index";
+import { Button, Icon, Dropdown } from "@/components/index";
 import { Article } from "@/types/article";
 import { toKoreanDateString } from "@/utils/date-util";
 
@@ -14,9 +16,17 @@ const ArticleHeader = ({ article }: ArticleHeaderProps) => {
     <div className="flex w-full flex-col gap-4 pb-[16px] tablet:pb-[28px]">
       <div className="flex items-center justify-between">
         <h2 className="text-xl font-bold text-blue-700">{article.title}</h2>
-        <Button variant="none">
-          <Icon icon="kebab" className="h-6 w-6" />
-        </Button>
+        <Dropdown
+          trigger={
+            <Button variant="none">
+              <Icon icon="kebab" className="h-6 w-6" />
+            </Button>
+          }
+          items={[
+            { label: "수정하기", onClick: () => {} },
+            { label: "삭제하기", onClick: () => {} },
+          ]}
+        />
       </div>
       <div className="flex items-center gap-2 border-b pb-3">
         <Image
