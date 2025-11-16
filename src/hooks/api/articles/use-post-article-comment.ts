7c31ece502eda51075cf27a30c0ff9047198f69a
junye0l <junyeolkim00@gmail.com>
2025-11-17 04:17:08 +0900

    feat : 자유게시판 댓글 작성시 토스트 표출

--- src/hooks/api/articles/use-post-article-comment.ts
+++ src/hooks/api/articles/use-post-article-comment.ts
@@ -1,16 +1,22 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import postArticlesComment from "@/api/articles/post-articles-comment";
+import useToast from "@/hooks/use-toast";
 
 export const usePostArticleComment = () => {
   const queryClient = useQueryClient();
+  const toast = useToast();
 
   return useMutation({
     mutationFn: ({ articleId, data }: { articleId: number; data: any }) =>
       postArticlesComment(articleId, data),
     onSuccess: (_, variables) => {
+      toast.success("댓글이 등록되었습니다 !");
       queryClient.invalidateQueries({
         queryKey: ["articleDetail", variables.articleId],
       });
+    },
+    onError: () => {
+      toast.error("댓글 등록에 실패했습니다 !");
     },
   });
 };
