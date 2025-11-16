ec7f609447ce291503b71a754ef0388a618cbe9b
JinHyuk Kim <dog33027@gmail.com>
2025-11-16 22:04:11 +0900

    feat: 임시 모바일, 태블릿 팀페이지 body 구현

---
+++ src/app/[groupId]/_components/team-body-mobile-tablet/team-body-mobile-tablet.tsx
@@ -0,0 +1,10 @@
+const TeamBodyMobileTablet = () => {
+  return (
+    <div className="flex-col border-[1px] border-black">
+      <p className="mb-[16px]">할 일 목록 (4개)</p>
+      <div className="flex-col flex-center"></div>
+    </div>
+  );
+};
+
+export default TeamBodyMobileTablet;
