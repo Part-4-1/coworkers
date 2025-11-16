ffeeafe509de59b2c6315500563ae47410ecb50f
junye0l <junyeolkim00@gmail.com>
2025-11-17 04:28:35 +0900

    feat : 자유게시판 게시글 수정, 삭제 권한 로직 적용

--- src/app/boards/[articleId]/_components/article-header/article-header.tsx
+++ src/app/boards/[articleId]/_components/article-header/article-header.tsx
@@ -5,6 +5,7 @@ import { Button, Icon, Dropdown } from "@/components/index";
 import { Article } from "@/types/article";
 import { toKoreanDateString } from "@/utils/date-util";
 import { useDeleteArticle } from "@/hooks/api/articles/use-delete-article";
+import { useGetUserInfoQuery } from "@/hooks/api/user/use-get-user-info-query";
 import useToast from "@/hooks/use-toast";
 
 interface ArticleHeaderProps {
@@ -14,11 +15,12 @@ interface ArticleHeaderProps {
 const ArticleHeader = ({ article }: ArticleHeaderProps) => {
   const router = useRouter();
   const Toast = useToast();
+  const { data: userInfo } = useGetUserInfoQuery();
   const createdAt = toKoreanDateString(article.createdAt);
   const { mutate: deleteArticleMutate, isPending } = useDeleteArticle();
 
-  console.log("article.id:", article.id);
-  console.log("article:", article);
+  const isWriter = userInfo?.id === article.writer.id;
+
   const handleDelete = () => {
     if (confirm("정말 삭제하시겠습니까?")) {
       deleteArticleMutate(article.id, {
@@ -37,20 +39,22 @@ const ArticleHeader = ({ article }: ArticleHeaderProps) => {
     <div className="flex w-full flex-col gap-4 pb-[16px] tablet:pb-[28px]">
       <div className="flex items-center justify-between">
         <h2 className="text-xl font-bold text-blue-700">{article.title}</h2>
-        <Dropdown
-          trigger={
-            <Button variant="none">
-              <Icon icon="kebab" className="h-6 w-6" />
-            </Button>
-          }
-          items={[
-            {
-              label: "수정하기",
-              onClick: () => router.push(`/boards/${article.id}/edit`),
-            },
-            { label: "삭제하기", onClick: handleDelete },
-          ]}
-        />
+        {isWriter && (
+          <Dropdown
+            trigger={
+              <Button variant="none">
+                <Icon icon="kebab" className="h-6 w-6" />
+              </Button>
+            }
+            items={[
+              {
+                label: "수정하기",
+                onClick: () => router.push(`/boards/${article.id}/edit`),
+              },
+              { label: "삭제하기", onClick: handleDelete },
+            ]}
+          />
+        )}
       </div>
       <div className="flex items-center gap-2 border-b pb-3">
         <div className="flex items-center gap-2">
