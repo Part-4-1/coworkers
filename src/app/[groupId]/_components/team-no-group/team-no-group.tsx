e585b5ca6319036dbd370b361da45c6a98e6688f
JinHyuk Kim <dog33027@gmail.com>
2025-11-16 22:05:42 +0900

    feat: group이 없을시 팀페이지 구현

---
+++ src/app/[groupId]/_components/team-no-group/team-no-group.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/index";
+import cn from "@/utils/clsx";
+import Image from "next/image";
+
+const TeamNoGroup = () => {
+  return (
+    <main
+      className={cn("flex h-screen w-full min-w-[200px] flex-col flex-center")}
+    >
+      <div className="relative h-[120px] w-[183px] tablet:h-[211px] tablet:w-[323px] pc:h-[264px] pc:w-[404px]">
+        <Image
+          src={"/team-page-no-group.png"}
+          alt="그룹없음"
+          fill
+          className="object-contain"
+        />
+      </div>
+      <div
+        className={cn(
+          "flex flex-col gap-[4px] text-md font-medium text-gray-800 flex-center",
+          "mt-[24px] tablet:mt-[32px] pc:text-lg"
+        )}
+      >
+        <p>아직 소속된 팀이 없습니다.</p>
+        <p>팀을 생성하거나 팀에 참여해보세요.</p>
+      </div>
+      <div className={cn("mt-[47px] flex flex-col gap-[8px] tablet:mt-[80px]")}>
+        <Button className="h-[48px] w-[186px]">팀 생성하기</Button>
+        <Button variant="outlined" className="h-[48px] w-[186px]">
+          팀 참여하기
+        </Button>
+      </div>
+    </main>
+  );
+};
+
+export default TeamNoGroup;
