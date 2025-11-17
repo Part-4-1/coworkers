0019a0455dbe0fe7e0c72135d36869a210fafb61
Imhwitae <ghkdgnlxo2@gmail.com>
2025-11-17 15:42:50 +0900

    fix: 모달 구조 수정

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -6,7 +6,7 @@ import usePrompt from "@/hooks/use-prompt";
 const Page = () => {
   const { Modal, openPrompt, closePrompt } = usePrompt(
     // <TaskModal groupId={3290} taskListId={4711} />,
-    <CreateTask />,
+    // <CreateTask handleClickBtn={closePrompt} />,
     true
   );
 
@@ -16,20 +16,22 @@ const Page = () => {
         <Button type="button" onClick={openPrompt}>
           모달 열기
         </Button>
-        <Modal />
+        <Modal>
+          <TaskModal groupId={3290} taskListId={4711} />
+        </Modal>
       </div>
     </div>
   );
 };
 
-const CreateTask = () => {
+const CreateTask = ({ handleClickBtn }: { handleClickBtn: () => void }) => {
   return (
     <div className="w-full gap-6 flex-col-center">
       <div className="gap-4 flex-col-center">
         <p>할 일 목록</p>
         <input type="text" placeholder="목록 명 입력..." />
       </div>
-      <Button>만들기</Button>
+      <Button onClick={handleClickBtn}>만들기</Button>
     </div>
   );
 };
