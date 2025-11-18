76908331174cf5d374308aa1d9b698b28a5773d0
JinHyuk Kim <dog33027@gmail.com>
2025-11-18 20:51:19 +0900

    feat: MemberProfileModal UI 구현

---
+++ src/app/[groupId]/_components/team-members-section/team-member/member-profile-modal.tsx
@@ -0,0 +1,34 @@
+import { Button, Profile } from "@/components";
+import { Member } from "@/types/members";
+import cn from "@/utils/clsx";
+
+interface MemberProfileModalProps {
+  onClick?: () => void;
+  member: Member;
+}
+
+const MemberProfileModal = ({ onClick, member }: MemberProfileModalProps) => {
+  return (
+    <div
+      className={cn(
+        "flex w-full flex-col px-[31.5px] pt-2 flex-center",
+        "tablet:w-[384px] tablet:pr-[60px]"
+      )}
+    >
+      <div className="mb-[24px]">
+        <Profile isMobileCircle={false} image={member.userImage} />
+      </div>
+      <div className="mb-[8px] text-md font-medium text-blue-700 tablet:text-lg">
+        {member.userName}
+      </div>
+      <div className="mb-[24px] text-xs text-gray-700 tablet:text-md">
+        {member.userEmail}
+      </div>
+      <div className="w-full">
+        <Button onClick={onClick}>이메일 복사하기</Button>
+      </div>
+    </div>
+  );
+};
+
+export default MemberProfileModal;
--- src/app/[groupId]/_components/team-members-section/team-member/team-member.tsx
+++ src/app/[groupId]/_components/team-members-section/team-member/team-member.tsx
@@ -2,7 +2,9 @@
 
 import { Icon, Profile } from "@/components";
 import useMediaQuery from "@/hooks/use-media-query";
+import usePrompt from "@/hooks/use-prompt";
 import { Member } from "@/types/members";
+import MemberProfileModal from "./member-profile-modal";
 
 interface TeamMemberProps {
   member: Member;
@@ -10,6 +12,11 @@ interface TeamMemberProps {
 
 const TeamMember = ({ member }: TeamMemberProps) => {
   const isMobile = useMediaQuery("(max-width: 744px)");
+  const { Modal, openPrompt, closePrompt } = usePrompt(true);
+
+  const handleCopyEmail = () => {
+    closePrompt();
+  };
 
   return (
     <div className="flex h-[68px] items-center justify-between rounded-[16px] bg-white py-[12px] pl-[16px] pr-[19px] tablet:h-[73px]">
@@ -29,12 +36,15 @@ const TeamMember = ({ member }: TeamMemberProps) => {
         <div className="truncate text-xs text-blue-700">{member.userEmail}</div>
       </div>
 
-      <div>
+      <div onClick={openPrompt}>
         <Icon
           icon="kebab"
           className="h-[20px] w-[20px] cursor-pointer text-gray-400"
         />
       </div>
+      <Modal>
+        <MemberProfileModal onClick={handleCopyEmail} member={member} />
+      </Modal>
     </div>
   );
 };
