8cbe037d830cd5488e40cb64cd5a9f6a4a6c8b88
Imhwitae <ghkdgnlxo2@gmail.com>
2025-11-20 16:24:27 +0900

    design: 완료된 리스트 배경색 추가

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -31,7 +31,8 @@ const List = ({
     <div
       className={cn(
         "mx-auto flex w-full flex-col gap-[10px] rounded-lg border border-gray-300 bg-white px-[14px] py-3 hover:bg-gray-100",
-        className
+        className,
+        doneAt && "bg-gray-50"
       )}
     >
       <div className="flex justify-between">
