6cf89d2bdcd73625efc006bdd69c8726a68203b5
junye0l <junyeolkim00@gmail.com>
2025-11-20 18:28:56 +0900

    feat : 팀 생성 성공시 해당하는 페이지로 리다이렉트 기능 추가

--- src/hooks/api/group/use-post-group.ts
+++ src/hooks/api/group/use-post-group.ts
@@ -1,16 +1,20 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import postGroup from "@/api/group/post-group";
 import useToast from "@/hooks/use-toast";
 
 const usePostGroup = () => {
   const queryClient = useQueryClient();
   const toast = useToast();
+  const router = useRouter();
 
   return useMutation({
     mutationFn: postGroup,
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("팀 생성이 성공했습니다 !");
+      queryClient.invalidateQueries({ queryKey: ["userInfo"] });
       queryClient.invalidateQueries({ queryKey: ["groups"] });
+      router.push(`/${data.id}`);
     },
     onError: () => {
       toast.error("팀 생성에 실패했습니다 !");
