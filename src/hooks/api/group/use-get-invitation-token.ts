97e4137372149ec4222ff5ad08c6be1986fd0120
JinHyuk Kim <dog33027@gmail.com>
2025-11-21 17:22:35 +0900

    refactor: 그룹 삭제 관련 api 호출 방식 수정

--- src/hooks/api/group/use-get-invitation-token.ts
+++ src/hooks/api/group/use-get-invitation-token.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 const useGetInvitationToken = (groupId: number) => {
   return useQuery({
-    queryKey: ["group", groupId],
+    queryKey: ["group", groupId, "invitationToken"],
     queryFn: () => getInvitationToken(groupId),
     staleTime: 1000 * 60 * 5,
     enabled: !!groupId,
