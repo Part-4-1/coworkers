d6d77aea999a645ef7e1efb4c2cbfbeeee257c88
apple <aa@aui-MacBookPro.local>
2025-11-23 00:46:35 +0900

    chore:카카오 로그인을 위한  넥스트 설정

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,18 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
       },
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 
