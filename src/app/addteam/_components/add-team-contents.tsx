2266ef58c610eee530cf7f46e6ab38f2aa923a91
junye0l <junyeolkim00@gmail.com>
2025-11-24 16:31:06 +0900

    fix : 팀 생성시 이미지 없을시 생성 오류 수정

--- src/app/addteam/_components/add-team-contents.tsx
+++ src/app/addteam/_components/add-team-contents.tsx
@@ -29,10 +29,10 @@ const AddTeamContents = () => {
     isUploading: isImageUploading,
   } = useProfileImageManager();
 
-  const onSubmit = (data: { name: string }) => {
+  const onSubmit = (data: { name: string; image?: string }) => {
     createGroup({
       name: data.name,
-      image: profileImage,
+      ...(profileImage && { image: profileImage }),
     });
   };
 
