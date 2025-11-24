2457edb73b87868371a742c5d3c7bc8954910e9a
Imhwitae <ghkdgnlxo2@gmail.com>
2025-11-24 21:05:40 +0900

    design: 날짜 선택 스켈레톤 추가

--- src/components/skeleton/list-skeleton/task-card-skeleton.tsx
+++
@@ -1,9 +0,0 @@
-import Skeleton from "react-loading-skeleton";
-
-const TaskCardSkeleton = () => {
-  return (
-    <Skeleton count={3} height={54} width={270} containerClassName="flex-1" />
-  );
-};
-
-export default TaskCardSkeleton;
