3069b9370f6558060472f3eb8bdcc3f5528b2e4d
junye0l <junyeolkim00@gmail.com>
2025-11-26 18:55:00 +0900

    refactor : 계정 설정 페이지 디자인 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -2,7 +2,7 @@ import UserSettingContents from "./_components/user-setting-contents";
 
 const MyPage = () => {
   return (
-    <div className="mt-[75px] flex-center tablet:mt-[176px] pc:mt-[156px]">
+    <div className="min-h-[calc(100vh-60px)] flex-center tablet:min-h-screen pc:min-h-screen">
       <div className="min-h-[556px] w-[343px] rounded-[20px] bg-white tablet:h-[745px] tablet:w-[550px] pc:w-[940px]">
         <UserSettingContents />
       </div>
