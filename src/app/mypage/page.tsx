03dc067ba18ed2e01d1303f5d01c5e829b8184fd
junye0l <junyeolkim00@gmail.com>
2025-11-27 15:06:34 +0900

    feat : 계정 설정 페이지 메타데이터 작업

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,4 +1,26 @@
+import type { Metadata } from "next";
 import UserSettingContents from "./_components/user-setting-contents";
+
+export const metadata: Metadata = {
+  title: "계정 설정",
+  description: "Coworkers 계정 설정 페이지",
+  openGraph: {
+    title: "계정 설정 | Coworkers",
+    description: "Coworkers 계정 설정 페이지",
+    type: "website",
+    url: "https://coworkes.com/mypage",
+    locale: "ko_KR",
+    siteName: "Coworkers",
+    images: [
+      {
+        url: "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/2449/open_graph.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Coworkers 계정 설정",
+      },
+    ],
+  },
+};
 
 const MyPage = () => {
   return (
