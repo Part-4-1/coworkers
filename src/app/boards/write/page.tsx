5da67c5a3d1efb4543d8a612f004bcb58f7fc686
junye0l <junyeolkim00@gmail.com>
2025-11-27 15:39:54 +0900

    feat : 자유게시판 게시글 작성 페이지 메타데이터 작업

--- src/app/boards/write/page.tsx
+++ src/app/boards/write/page.tsx
@@ -1,5 +1,27 @@
+import type { Metadata } from "next";
 import cn from "@/utils/clsx";
 import ArticleWriteContents from "./_components/article-write-contents/article-write-contents";
+
+export const metadata: Metadata = {
+  title: "게시글 작성",
+  description: "Coworkers 자유게시판에 새로운 글을 작성하세요",
+  openGraph: {
+    title: "게시글 작성 | Coworkers",
+    description: "Coworkers 자유게시판에 새로운 글을 작성하세요",
+    type: "website",
+    url: "https://coworkes.com/boards/write",
+    locale: "ko_KR",
+    siteName: "Coworkers",
+    images: [
+      {
+        url: "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Coworkers/user/2449/open_graph.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Coworkers 게시글 작성",
+      },
+    ],
+  },
+};
 
 const Page = () => {
   return (
