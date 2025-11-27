a302e1143c3a3538b983a69cc462941b791f8d8d
junye0l <junyeolkim00@gmail.com>
2025-11-28 00:08:20 +0900

    feat : 채널톡 서비스 구현 완료

--- src/providers.tsx
+++ src/providers.tsx
@@ -6,6 +6,8 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useEffect, useState } from "react";
+import ChannelService from "./components/channel-talk";
 
 function makeQueryClient() {
   return new QueryClient({
@@ -30,6 +32,14 @@ function getQueryClient() {
 
 const QueryProviders = ({ children }: { children: React.ReactNode }) => {
   const queryClient = getQueryClient();
+
+  useEffect(() => {
+    ChannelService.loadScript();
+
+    ChannelService.boot({
+      pluginKey: "4be26d42-5350-46c1-b390-93e0d142750e",
+    });
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
