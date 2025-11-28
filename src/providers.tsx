186a38b76555e7cbab67bfada66e48ef4861a31e
junye0l <junyeolkim00@gmail.com>
2025-11-28 15:42:26 +0900

    refactor : 채널톡, 플로팅 버튼 위치 수정 및 할 일 목록 페이지 채널톡 숨김 기능

--- src/providers.tsx
+++ src/providers.tsx
@@ -7,6 +7,7 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import ChannelService from "./components/channel-talk";
 
 function makeQueryClient() {
@@ -32,14 +33,20 @@ function getQueryClient() {
 
 const QueryProviders = ({ children }: { children: React.ReactNode }) => {
   const queryClient = getQueryClient();
+  const pathname = usePathname();
 
   useEffect(() => {
     ChannelService.loadScript();
+  }, []);
+
+  useEffect(() => {
+    const isTasklistPage = pathname?.includes("/tasklist");
 
     ChannelService.boot({
       pluginKey: "4be26d42-5350-46c1-b390-93e0d142750e",
+      hideChannelButtonOnBoot: isTasklistPage,
     });
-  }, []);
+  }, [pathname]);
 
   return (
     <QueryClientProvider client={queryClient}>
