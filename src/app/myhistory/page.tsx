687ba1a6c5f7c42d3bd4176d212ff0d486ead2a0
Imhwitae <ghkdgnlxo2@gmail.com>
2025-12-01 15:33:30 +0900

    feat: 마이 히스토리 목록 없을 때 이미지 추가

--- src/app/myhistory/page.tsx
+++ src/app/myhistory/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
 
   return (
     <div className="flex h-full w-full max-w-[1120px] flex-col gap-4 py-[50px] tablet:gap-[34px] tablet:px-[26px] pc:gap-12">
-      <div className="mx-4 flex flex-1 flex-col overflow-hidden rounded-[20px] bg-white px-5 pb-[52px] pt-[33px] tablet:mx-0 tablet:px-[30px] pc:px-[37px]">
+      <div className="relative mx-4 flex flex-1 flex-col overflow-hidden rounded-[20px] bg-white px-5 pb-[52px] pt-[33px] tablet:mx-0 tablet:px-[30px] pc:px-[37px]">
         <div className="flex h-full flex-col gap-[27px] overflow-hidden">
           <p className="text-2lg font-bold">마이 히스토리</p>
           <HistoryList
